de2f72a0b41bbff3d0a1e9d0cd2ed99c37254726
Artem Tiutiunnyk <artem@qwil.io>
2019-01-17 17:36:33 +0200

    Added default value for allow to export

--- lib/open_file.dart
+++ lib/open_file.dart
@@ -5,8 +5,11 @@ import 'package:flutter/services.dart';
 class OpenFile {
   static const MethodChannel _channel = const MethodChannel('open_file');
 
-  static Future<String> open(filePath, allowToExport) async {
-    Map<String, dynamic> map = {"file_path": filePath, "allow_to_export": allowToExport};
+  static Future<String> open(filePath, {allowToExport = false}) async {
+    Map<String, dynamic> map = {
+      "file_path": filePath,
+      "allow_to_export": allowToExport
+    };
     return await _channel.invokeMethod('open_file', map);
   }
 }
